83f28de6fc498c1fd5e099af6c39cbc474c091d7
mperry3 <mperry3@wisc.edu>
2019-03-24 12:49:05 -0500

    Ticket closed: alarm page shows current time and has a button that hits a stubbed out method in alarmActions

--- src/screens/AlarmScreen.js
+++ src/screens/AlarmScreen.js
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { View, Text, Button } from 'react-native';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import { alarmOff } from '../store/actions/alarmActions';
+
 import { GlobalStyles, Colors } from '../constants';
 
 class AlarmScreen extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      time: moment().format('LT'),
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      time: moment().format('LT'),
+    });
   }
 
   render() {
+    const { time } = this.state;
+    const { navigation, turnAlarmOff } = this.props;
+    const { navigate } = navigation;
     return (
       <View style={GlobalStyles.centerChildrenXY}>
-        <View
-          style={[
-            {
-              backgroundColor: Colors.primary,
-              width: 48,
-              height: 48,
-              borderRadius: 8,
-            },
-            GlobalStyles.margin,
-          ]}
+        <Text style={[GlobalStyles.h1, GlobalStyles.margin, { color: Colors.primary }]}>
+          { time }
+        </Text>
+        <View style={{ height: 8, width: 8 }} />
+        <Button
+          title="Turn Off Alarm"
+          color={Colors.darkGray}
+          onPress={() => turnAlarmOff(navigate)}
         />
-        <Text style={[GlobalStyles.h1, GlobalStyles.margin]}>Alarm</Text>
       </View>
     );
   }
 }
 
-export default AlarmScreen;
+const mapDispatchToProps = dispatch => ({
+  turnAlarmOff: (navigate) => { dispatch(alarmOff(navigate('Home'))); },
+});
+
+AlarmScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+  turnAlarmOff: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(AlarmScreen);
