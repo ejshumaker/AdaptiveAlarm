664495a6ef05ae5270bb6616d8373bbe7e474cfd
adam <adamweintraut14@gmail.com>
2019-03-28 18:29:03 -0500

    removing autocompletescreen2

--- src/screens/AutocompleteScreen.js
+++ src/screens/AutocompleteScreen.js
@@ -1,116 +1,24 @@
 /**
   * Uses Google Maps API to autocomplete search entries.
   * Code modified from Stefan Hyltoft (https://github.com/Hyllesen)
+  * @weinoh 03-26-2019
   * @ejshumaker 03-24-2019
 */
 
-import React, { Component } from "react";
-import {
-  StyleSheet,
-  TextInput,
-  View,
-  Text,
-  TouchableHighlight,
-  Keyboard,
-  Image
-} from "react-native";
-import { Colors, GlobalStyles } from '../constants';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import _ from "lodash";
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { GlobalStyles } from '../constants';
 
-import { GoogleAutoComplete } from 'react-native-google-autocomplete';
+import Autocomplete from '../components/Autocomplete';
 
-class AutocompleteScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      error: "",
-      locationPredictions: []
-    };
-    this.onChangeDestinationDebounced = _.debounce(
-      this.onChangeDestination,
-      1000
-    );
-  }
-
-  async onChangeDestination(destination) {
-    const API_KEY = 'AIzaSyBpwrz2oV29sjAAKj2l6BIb6l5luzDIsIw';
-    // hard coded starting location to Memorial Union, need to use current location later
-    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${API_KEY}&input={${destination}}&location=43.0762,-89.3998&radius=100`;
-    const result = await fetch(url);
-    const jsonResult = await result.json();
-    this.setState({
-      locationPredictions: jsonResult.predictions
-    });
-  }
-
-  async pressedPrediction(prediction) {
-    Keyboard.dismiss();
-    console.log(prediction.description)
-    this.setState({
-      locationPredictions: [],
-      destination: prediction.description
-    });
-    Keyboard;
-    const API_KEY = 'AIzaSyBpwrz2oV29sjAAKj2l6BIb6l5luzDIsIw';
-    const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${prediction.place_id}&key=${API_KEY}`;
-    return new Promise((resolve, reject) => {
-      fetch(url)
-        .then(response => response.json())
-        .then((json) => {
-          if (json.status !== 'OK') {
-            const errorMessage = json.error_message || 'Unknown error';
-            reject(errorMessage);
-          }
-          // TODO: need to parse json result to get location
-          // console.log(json);
-          // const { lat } = json.location;
-          // const { lng } = json.location;
-          // console.log(`lat: ${lat} long: ${lng}`);
-          // resolve(`{lat: ${lat}, lng: ${lng}}`);
-        });
-    });
-  }
+// eslint-disable-next-line
+export default class AutocompleteScreen extends Component {
 
   render() {
-  const locationPredictions = this.state.locationPredictions.map(
-    prediction => (
-      <TouchableHighlight
-        key={prediction.id}
-        onPress={() => this.pressedPrediction(prediction)}
-      >
-        <Text style={GlobalStyles.searchSuggestions}>
-          {prediction.description}
-        </Text>
-      </TouchableHighlight>
-    )
-  );
-
     return (
       <View style={GlobalStyles.centerChildrenXY}>
-        <TextInput
-          style={GlobalStyles.destinationInput}
-          placeholder="Enter destination"
-          onChangeText={destination => {
-            this.onChangeDestinationDebounced(destination);
-          }}
-          value={this.state.destination}
-        />
-        {locationPredictions}
-        <Image
-          style={{marginTop: 5}}
-          source={require('../assets/powered_by_google_on_non_white.png')}
-        />
+        <Autocomplete />
       </View>
     );
   }
 }
-
-AutocompleteScreen.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func.isRequired,
-  }).isRequired,
-};
-
-export default AutocompleteScreen;
