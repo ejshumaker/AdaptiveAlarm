fe0d4dda9967cb7a5d148ba703da867232610429
Eric Schirtzinger <eschirtzinger@gmail.com>
2019-04-15 22:52:09 -0500

    Ejected and successfully ran in Xcode and AndroidStudio

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expe26fb07a0def452b9a88e3b846db0761://192.168.86.35:19000";
+
+}
