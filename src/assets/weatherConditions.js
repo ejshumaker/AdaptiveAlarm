f7cbff627dd9b39dbfd7b0ed6e2e1eb65b743b5b
Eric <eshumaker@wisc.edu>
2019-04-24 11:47:29 -0500

    Displaying hardcoded text on home page, not yet hooked up

---
+++ src/assets/weatherConditions.js
@@ -0,0 +1,26 @@
+export const weatherConditions = {
+  Rain: {
+    icon: 'weather-rainy'
+  },
+  Clear: {
+    icon: 'weather-sunny'
+  },
+  Thunderstorm: {
+    icon: 'weather-lightning'
+  },
+  Clouds: {
+    icon: 'weather-cloudy'
+  },
+  Snow: {
+    icon: 'weather-snowy'
+  },
+  Drizzle: {
+    icon: 'weather-hail'
+  },
+  Haze: {
+    icon: 'weather-hail'
+  },
+  Mist: {
+    icon: 'weather-fog'
+  }
+};
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -5,8 +5,11 @@ import {
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import AnalogClock from '../components/AnalogClock';
 import { Buttons, StatusBarBackground } from '../components';
+import Alarm from '../custom_modules/Alarm';
+import { WEATHER_KEY } from '../../keys';
 
 import { userSetAlarmStatus } from '../store/actions/userActions';
 import { GlobalStyles, Colors } from '../constants';
@@ -16,12 +19,26 @@ import { UserIcon } from '../icons/user';
 import { MenuIcon } from '../icons/menu';
 
 class MainScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      temperature: 69,
+      weather: '',
+      weatherLoading: true,
+    };
+  }
+
+  // didFocus = () => {
+  //   this.weatherTest();
+  // }
+
   hasAlarmView() {
     const {
       // values
       alarmTime,
       loading,
     } = this.props;
+    const { weatherLoading, temperature, weather } = this.state;
     const hour = !loading ? moment(alarmTime).format('hh') : '0';
     const min = !loading ? moment(alarmTime).format('mm') : '00';
     const meridian = !loading ? moment(alarmTime).format('a') : '- -';
@@ -51,11 +68,20 @@ class MainScreen extends Component {
             {meridian.toUpperCase()}
           </Text>
         </Text>
+        {!weatherLoading ? null : <Text>{temperature}</Text>}
         <Text style={
           [GlobalStyles.month, { color: Colors.white, marginLeft: 130 }]
         }
         >
+          <Text style={GlobalStyles.date}>
+            {temperature}
+          </Text>
           <Text>
+            {'\u2109   '}
+          </Text>
+          <MaterialCommunityIcons size={20} name="weather-cloudy" color={Colors.white} />
+          <Text>
+            {'  '}
             {month.toUpperCase()}
             {' '}
           </Text>
@@ -109,6 +135,7 @@ class MainScreen extends Component {
       navigation,
       alarmId,
     } = this.props;
+    const { temperature, weather, weatherLoading } = this.state;
     const { navigate } = navigation;
     return (
       <View>
@@ -124,6 +151,16 @@ class MainScreen extends Component {
           textColor={Colors.white}
           onPress={() => navigate('Home')}
         />
+        <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center' }}>
+          <Buttons
+            title="Get Weather"
+            backgroundColor={Colors.primary}
+            textColor={Colors.black}
+            onPress={() => {
+              this.weatherTest();
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -167,6 +204,31 @@ class MainScreen extends Component {
         </TouchableOpacity>
       </View>
     );
+  }
+
+  async weatherTest() {
+    let temperature = '';
+    let weather = '';
+    let lat = 0;
+    let lon = 0;
+    await Alarm.getCurrentLocation()
+      .then((response) => {
+        const loc = response;
+        const locArr = loc.split(', ');
+        [lat] = locArr;
+        [lon] = locArr;
+      });
+    await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&APPID=${WEATHER_KEY}`)
+      .then(response => response.json())
+      .then((json) => {
+        temperature = json.main.temp;
+        weather = json.weather[0].main;
+        this.setState({ temperature, weather, weatherLoading: false });
+      });
+    console.log('==================');
+    console.log(this.state.temperature);
+    console.log(this.state.weather);
+    console.log('==================');
   }
 
   render() {
